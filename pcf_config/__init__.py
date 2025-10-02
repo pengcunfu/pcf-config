bd39c6ed3341029ca7668f71cd8bac17d3767ed9
pengcunfu <3173484026@qq.com>
2025-10-02 18:55:39 +0800

    Fix pyproject.toml license field format for v1.1.3

--- pcf_config/__init__.py
+++ pcf_config/__init__.py
@@ -8,7 +8,7 @@ with support for nested keys, default values, and read/write operations.
 
 from .config import Config
 
-__version__ = "1.1.2"
+__version__ = "1.1.3"
 __author__ = "pengcunfu"
 __email__ = "3173484026@qq.com"
 __description__ = "A simple YAML configuration management library"
